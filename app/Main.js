19d37c8b0b3cdf93c4bb19dc3d77c3b7cec31486
Guilherme Simão Costa <Gui_SC@outlook.com>
2020-06-05 13:21:58 -0300

    Adding Router

--- app/Main.js
+++ app/Main.js
@@ -1,5 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+
 const useState = React.useState;
 const useEffect = React.useEffect;
 
@@ -7,14 +9,26 @@ const useEffect = React.useEffect;
 import Header from "./components/Header";
 import HomeGuest from "./components/HomeGuest";
 import Footer from "./components/Footer";
+import About from "./components/About";
+import Terms from "./components/Terms";
 
 function Main() {
   return (
-    <>
+    <BrowserRouter>
       <Header />
-      <HomeGuest />
+      <Switch>
+        <Route path="/" exact>
+          <HomeGuest />
+        </Route>
+        <Route path="/about-us" exact>
+          <About />
+        </Route>
+        <Route path="/terms" exact>
+          <Terms />
+        </Route>
+      </Switch>
       <Footer />
-    </>
+    </BrowserRouter>
   );
 }
 
